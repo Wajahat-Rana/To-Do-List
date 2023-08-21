d91d4b86dba8081bc92862378dfa42c1797f5e1e
Rana Wajahat Saleem <128230044+Wajahat-Rana@users.noreply.github.com>
2023-08-21 17:12:08 +0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,29 @@
+let task_btn = document.getElementById('task_button');
+let ul_list = document.getElementById('ul_list');
+let reset_button = document.getElementById('reset_button');
+
+console.log('To-Do-List');
+
+task_btn.addEventListener('click',()=>{
+    let user_input = document.getElementById('task');
+    console.log('Added To List');
+    const new_task = document.createElement('li');
+    let checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    const task_data = document.createTextNode(user_input.value);
+    new_task.appendChild(checkbox);
+    new_task.appendChild(task_data);
+    ul_list.appendChild(new_task);
+    user_input.value = '';
+
+    checkbox.addEventListener('change', () => {
+        if (checkbox.checked) {
+            new_task.classList.add('completed');
+        } else {
+            new_task.classList.remove('completed');
+        }
+    });
+});
+reset_button.addEventListener('click', () => {
+    ul_list.innerHTML = ''; // Clear the list
+});
